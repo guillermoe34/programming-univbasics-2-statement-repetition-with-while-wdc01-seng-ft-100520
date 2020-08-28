74234e4dfd9e88b7cdf2833efa789b4793981fbd
Guillermo Chang <e.guillermo@rocketmail.com>
2020-08-28 15:46:32 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,6 @@
 magic_exit_number = 0
-count = 
+count = -0
 while count < 10 && count != magic_exit_number do
   puts "I am the #{count}, I love to count!" # Work
-  count = count -= 1
+  count = count + 1
 end
